eb12efbd077c814cbe8187702fb48d65e83c781e
Nilavann <nalasivam1997@gmail.com>
2018-12-05 19:25:49 +0530

    Update Project Euler #2: Even Fibonacci numbers.py
    Added additional description

--- Project Euler/Project Euler #2: Even Fibonacci numbers.py
+++ Project Euler/Project Euler #2: Even Fibonacci numbers.py
@@ -3,11 +3,13 @@
 import sys
 import math
 
-# we every 3 element divisible by 2
+# we know that every 3 element in Fibonacci series is divisible by 2
 # 0,2,8,34
 # 4 * 8   + 2 = 34
 # 4 * 34  + 8 = 144
 # 4 * 144 + 34 = goes on
+# 4 * n2  + n1 = n3
+# 4 * n3  + n2 = n4
 t = int(input().strip())
 for a0 in range(t):
     n = int(input().strip())
